2636ebab10269bccfdac8a3ba2dd60706974c3a1
DaCyclops <abcyclops@gmail.com>
2015-09-02 06:11:20 +1000

    Added Productivity Module support for recipies

--- prototypes/GDIW.lua
+++ prototypes/GDIW.lua
@@ -97,6 +97,13 @@ data:extend(
   }
 })
 
+for km, vm in pairs(data.raw.module) do
+  if vm.name:find("productivity%-module") and vm.limitation then
+    for _, recipe in ipairs({"advanced-oil-processing-GDIW","heavy-oil-cracking-GDIW","light-oil-cracking-GDIW","sulfur-GDIW"}) do
+      table.insert(vm.limitation, recipe)
+    end
+  end
+end
 
 table.insert(data.raw["technology"]["advanced-oil-processing"].effects,{type="unlock-recipe",recipe="advanced-oil-processing-GDIW"})
 table.insert(data.raw["technology"]["advanced-oil-processing"].effects,{type="unlock-recipe",recipe="heavy-oil-cracking-GDIW"})
