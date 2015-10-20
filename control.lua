b36a6d7c0e2cbf67138e33c2413f6a9b69379bed
DaCyclops <abcyclops@gmail.com>
2015-10-20 12:22:04 +1100

    Version 1.0.2 - Attempted Migration to Factorio 0.12.11

--- control.lua
+++ control.lua
@@ -1,12 +1,10 @@
-local mod_version="1.0.0"
 
-local function onInit()
-   onLoad()   
-end    
 
-local function onLoad()
-  if global.GDIW==nil or global.GDIW.version ~= mod_version then
-    --unlock if needed
+
+local function onConfigChange(cDat)
+  
+  if cDat.mod_changes ~= nil and cDat.mod_changes["GDIW"] ~= nil and cDat.mod_changes["GDIW"].old_version == nil then
+   -- Mod added 
     for _,force in pairs(game.forces) do
       force.reset_recipes()
       force.reset_technologies()
@@ -27,15 +25,47 @@ local function onLoad()
       end
 
     end
+    
+  end 
+
+  if cDat.mod_changes ~= nil and cDat.mod_changes["GDIW"] ~= nil and cDat.mod_changes["GDIW"].old_version ~= nil then
+   -- Mod updated or removed
+    for _,force in pairs(game.forces) do
+      force.reset_recipes()
+      force.reset_technologies()
+
+      local techs=force.technologies
+      local recipes=force.recipes
+
+      if techs["advanced-oil-processing"].researched then
+        recipes["advanced-oil-processing-GDIW"].enabled=true
+        recipes["heavy-oil-cracking-GDIW"].enabled=true
+        recipes["light-oil-cracking-GDIW"].enabled=true
+      end
+      if techs["sulfur-processing"].researched then
+        recipes["sulfur-GDIW"].enabled=true
+      end
+      if techs["flame-thrower"].researched then
+        recipes["flame-thrower-ammo-GDIW"].enabled=true
+      end      
+      
+    end
+
   end
+
 end
 
-local function onSave()
-  global.GDIW={version=mod_version}
+
+local function onInit()
+  -- Nothing to do now
+end    
+
+local function onLoad()
+  -- Nothing to do now
 end
 
 
 script.on_init(onInit)
 script.on_load(onLoad)
 
---game.on_save(onSave)
+script.on_configuration_changed(onConfigChange(data))
