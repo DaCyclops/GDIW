e514eca3c002343444a9bce812c8067006a3599a
DaCyclops <abcyclops@gmail.com>
2015-10-21 21:39:57 +1100

    Version 1.0.2 - Factorio 0.12.11 Migration

--- control.lua
+++ control.lua
@@ -1,9 +1,9 @@
 
 
 
-local function onConfigChange(cDat)
-  
-  if cDat.mod_changes ~= nil and cDat.mod_changes["GDIW"] ~= nil and cDat.mod_changes["GDIW"].old_version == nil then
+ -- Start OnLoad/OnInit/OnConfig events
+script.on_configuration_changed( function(data)
+  if data.mod_changes ~= nil and data.mod_changes["GDIW"] ~= nil and data.mod_changes["GDIW"].old_version == nil then
    -- Mod added 
     for _,force in pairs(game.forces) do
       force.reset_recipes()
@@ -28,7 +28,7 @@ local function onConfigChange(cDat)
     
   end 
 
-  if cDat.mod_changes ~= nil and cDat.mod_changes["GDIW"] ~= nil and cDat.mod_changes["GDIW"].old_version ~= nil then
+  if data.mod_changes ~= nil and data.mod_changes["GDIW"] ~= nil and data.mod_changes["GDIW"].old_version ~= nil then
    -- Mod updated or removed
     for _,force in pairs(game.forces) do
       force.reset_recipes()
@@ -53,19 +53,16 @@ local function onConfigChange(cDat)
 
   end
 
-end
+end)
 
 
-local function onInit()
+script.on_init(function()
   -- Nothing to do now
-end    
+end)   
+  
+script.on_load(function()
+  --Nothing to Do Now  
+end)
+-- End OnLoad/OnInit/OnConfig events
 
-local function onLoad()
-  -- Nothing to do now
-end
 
-
-script.on_init(onInit)
-script.on_load(onLoad)
-
-script.on_configuration_changed(onConfigChange(data))
