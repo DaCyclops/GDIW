7ff10eba6de8b2f36bc5a38e484dd4a83fa4876e
Trent Petersen <trentpetersen.523@gmail.com>
2016-02-27 14:13:43 -0600

    Add Bobs Crude Oil Processing
    This adds an optional dependency on Bobs Plates to allow automatic
    compatibility.

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -4,5 +4,9 @@ for km, vm in pairs(data.raw.module) do
     for _, recipe in ipairs({"basic-oil-processing-GDIW-3","advanced-oil-processing-GDIW","advanced-oil-processing-GDIW-2","advanced-oil-processing-GDIW-3","heavy-oil-cracking-GDIW","light-oil-cracking-GDIW","sulfur-GDIW"}) do
       table.insert(vm.limitation, recipe)
     end
+
+    if data.raw["recipe"]["bob-oil-processing"] then
+      table.insert(vm.limitation, "bob-oil-processing-GDIW")
+    end
   end
 end
