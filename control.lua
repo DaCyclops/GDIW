6a0be0084ae1fcd0da1beba1b5ac99fd2ed0fd6a
DaCyclops <abcyclops@gmail.com>
2016-11-18 14:16:22 +1100

    Potential final version 0.14.10
    removed old prototypes, migrated to auto-prototype method

--- control.lua
+++ control.lua
@@ -1,4 +1,18 @@
+function recheck_all_recipes (rf) 
 
+      for _, research in pairs(rf.technologies) do
+        if research.researched then
+         if research.effects then
+           for _, effect in pairs (research.effects) do
+             if effect.type == "unlock-recipe" then
+               rf.recipes[effect.recipe].enabled = true
+             end
+           end
+         end
+        end
+      end
+
+end
 
 
  -- Start OnLoad/OnInit/OnConfig events
@@ -9,29 +23,7 @@ script.on_configuration_changed( function(data)
       force.reset_recipes()
       force.reset_technologies()
 
-      local techs=force.technologies
-      local recipes=force.recipes
-
-      if techs["oil-processing"].researched then
-        recipes["basic-oil-processing-GDIW-3"].enabled=true
-      end
-      if techs["advanced-oil-processing"].researched then
-        recipes["advanced-oil-processing-GDIW"].enabled=true
-        recipes["advanced-oil-processing-GDIW-2"].enabled=true
-        recipes["advanced-oil-processing-GDIW-3"].enabled=true
-        recipes["heavy-oil-cracking-GDIW"].enabled=true
-        recipes["light-oil-cracking-GDIW"].enabled=true
-        if recipes["bob-oil-processing-GDIW"] then
-          recipes["bob-oil-processing-GDIW"].enabled=true
-        end
-      end
-      if techs["sulfur-processing"].researched then
-        recipes["sulfur-GDIW"].enabled=true
-      end
-      if techs["flame-thrower"].researched then
-        recipes["flame-thrower-ammo-GDIW"].enabled=true
-      end
-
+      recheck_all_recipes(force)
     end
     
   end 
@@ -41,31 +33,10 @@ script.on_configuration_changed( function(data)
     for _,force in pairs(game.forces) do
       force.reset_recipes()
       force.reset_technologies()
-
-      local techs=force.technologies
-      local recipes=force.recipes
-
-      if techs["oil-processing"].researched then
-        recipes["basic-oil-processing-GDIW-3"].enabled=true
-      end
-      if techs["advanced-oil-processing"].researched then
-        recipes["advanced-oil-processing-GDIW"].enabled=true
-        recipes["advanced-oil-processing-GDIW-2"].enabled=true
-        recipes["advanced-oil-processing-GDIW-3"].enabled=true
-        recipes["heavy-oil-cracking-GDIW"].enabled=true
-        recipes["light-oil-cracking-GDIW"].enabled=true
-        if recipes["bob-oil-processing-GDIW"] then
-          recipes["bob-oil-processing-GDIW"].enabled=true
-        end
-      end
-      if techs["sulfur-processing"].researched then
-        recipes["sulfur-GDIW"].enabled=true
-      end
-      if techs["flame-thrower"].researched then
-        recipes["flame-thrower-ammo-GDIW"].enabled=true
-      end      
       
+      recheck_all_recipes(force)
     end
+
 
   end
 
--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -4,7 +4,103 @@ GDIWworklistIn = {}
 GDIWworklistOut = {}
 GDIWworklistBoth = {}
 GDIWproductivity = {}
+GDIWresearch = {}
 
+-- function for doing prototypes
+function GDIWdoprototype(GDIWwl, isIn, isOut )
+  for _, vin in pairs(GDIWwl) do
+    -- Determine new names
+    if isIn and isOut then
+    suffix = "GDIW-BR"
+    elseif isIn then
+    suffix = "GDIW-IR"
+    elseif isOut then
+    suffix = "GDIW-OR"
+    else
+    suffix = "GDIW-AR"
+    end
+    newName = vin.r .. "-" .. suffix
+    -- copy table
+    data.raw.recipe[newName] = util.table.deepcopy(data.raw.recipe[vin.r])
+    
+    vro = data.raw.recipe[vin.r]
+    vrn = data.raw.recipe[newName]
+    -- fix names (and make enabled for testing)  
+    vrn.name = newName
+    localised_name = {"recipe-name." .. vro.name," ","GDIW." .. suffix}
+    --vrn.enabled = true
+    
+    ingbuild = {}
+    newing = {}
+    fluidflip = {}
+    
+    if isIn then
+    sortcount = 0
+    for _, vri in pairs(vrn.ingredients) do
+      --Flip Input fluids
+      sortcount = sortcount + 1
+      vri.sortorder = sortcount
+      if vri.type == "fluid" then
+        vri.sortorder = 1000 - sortcount
+      end
+    end  
+    table.sort(vrn.ingredients, function(a,b) return a.sortorder<b.sortorder end)
+    end
+    
+    if isOut then
+    sortcount = 0
+    for _, vrr in pairs(vrn.results) do
+      --Flip Output fluids
+      sortcount = sortcount + 1
+      vrr.sortorder = sortcount
+      if vrr.type == "fluid" then
+        vrr.sortorder = 1000 - sortcount
+      end
+    end  
+    table.sort(vrn.results, function(a,b) return a.sortorder<b.sortorder end)
+    end
+  
+    newicons = {}
+    --grab old icon(s)
+    if vro.icon then
+      table.insert(newicons,{icon=vro.icon})
+      oldicon = vro.icon
+    elseif vro.icons then
+      newicons = util.table.deepcopy(vro.icons)
+    else
+      table.insert(newicons,{icon="__GDIW__/graphics/placeholder.png"})  
+    end
+    -- add overlay and use
+    if isIn and isOut then
+      table.insert(newicons,{icon = "__GDIW__/graphics/reverse-overlay-both.png"})
+    elseif isIn then
+      table.insert(newicons,{icon = "__GDIW__/graphics/reverse-overlay-input.png"})
+    elseif isOut then
+      table.insert(newicons,{icon = "__GDIW__/graphics/reverse-overlay-output.png"})
+    else
+      table.insert(newicons,{icon = "__GDIW__/graphics/reverse-overlay-both.png"})
+    end
+    
+    vrn.icons = newicons
+    
+    -- handle productivity modules
+    if vin.prod then
+      table.insert(GDIWproductivity,newName)
+    end
+  
+  if not GDIWresearch[vin.r] then
+    GDIWresearch[vin.r] = {}
+  end
+  
+  table.insert(GDIWresearch[vin.r], newName)
+  
+  end
+
+end --end function
+
+
+
+-- Find what needs to be done
 for kr, vr in pairs(data.raw.recipe) do
   --For each recipie
   GDIWfincount = 0
@@ -56,167 +152,30 @@ for kr, vr in pairs(data.raw.recipe) do
 end
 
 
-for _, vin in pairs(GDIWworklistIn) do
-  -- Determine new names
-  newName = vin.r .. "-GDIW-IR"
-  -- copy table
-  data.raw.recipe[newName] = util.table.deepcopy(data.raw.recipe[vin.r])
-  
-  vro = data.raw.recipe[vin.r]
-  vrn = data.raw.recipe[newName]
-  -- fix names (and enabled for testing)  
-  vrn.name = newName
-  localised_name = {{"recipe-name." .. vro.name},"GDIW.input-reversed"}
-  vrn.enabled = true
-  
-  ingbuild = {}
-  newing = {}
-  fluidflip = {}
-  
-  sortcount = 0
-  for _, vri in pairs(vrn.ingredients) do
-    --Flip Input fluids
-    sortcount = sortcount + 1
-    vri.sortorder = sortcount
-    if vri.type == "fluid" then
-      vri.sortorder = 1000 - sortcount
-    end
-  end  
-  table.sort(vrn.ingredients, function(a,b) return a.sortorder<b.sortorder end)
-  
-  newicons = {}
-  --grab old icon(s)
-  if vro.icon then
-    table.insert(newicons,{icon=vro.icon})
-    oldicon = vro.icon
-  elseif vro.icons then
-    newicons = util.table.deepcopy(vro.icons)
-  else
-    table.insert(newicons,{icon="__GDIW__/graphics/placeholder.png"})  
-  end
-  -- add overlay and use
-  table.insert(newicons,{icon = "__GDIW__/graphics/reverse-overlay-input.png"})
-  vrn.icons = newicons
-  
-  -- handle productivity modules
-  if vin.prod then
-    table.insert(GDIWproductivity,newName)
-  end
-  
-end
 
-for _, vin in pairs(GDIWworklistOut) do
-  -- Determine new names
-  newName = vin.r .. "-GDIW-OR"
-  -- copy table
-  data.raw.recipe[newName] = util.table.deepcopy(data.raw.recipe[vin.r])
-  
-  vro = data.raw.recipe[vin.r]
-  vrn = data.raw.recipe[newName]
-  -- fix names (and enabled for testing)  
-  vrn.name = newName
-  localised_name = {{"recipe-name." .. vro.name},"GDIW.output-reversed"}
-  vrn.enabled = true
-  
-  ingbuild = {}
-  newing = {}
-  fluidflip = {}
-  
-  sortcount = 0
-  for _, vrr in pairs(vrn.results) do
-    --Flip Output fluids
-    sortcount = sortcount + 1
-    vrr.sortorder = sortcount
-    if vrr.type == "fluid" then
-      vrr.sortorder = 1000 - sortcount
-    end
-  end  
-  table.sort(vrn.results, function(a,b) return a.sortorder<b.sortorder end)
-  
-  
-  newicons = {}
-  --grab old icon(s)
-  if vro.icon then
-    table.insert(newicons,{icon=vro.icon})
-    oldicon = vro.icon
-  elseif vro.icons then
-    newicons = util.table.deepcopy(vro.icons)
-  else
-    table.insert(newicons,{icon="__GDIW__/graphics/placeholder.png"})  
-  end
-  -- add overlay and use
-  table.insert(newicons,{icon = "__GDIW__/graphics/reverse-overlay-output.png"})
-  vrn.icons = newicons
-  
-  -- handle productivity modules
-  if vin.prod then
-    table.insert(GDIWproductivity,newName)
-  end
-  
-end
+-- Add all prototypes via function
+-- GDIWdoprototype(GDIWwl, isIn, isOut )
+GDIWdoprototype(GDIWworklistIn, true, false )
+GDIWdoprototype(GDIWworklistOut, false, true )
+GDIWdoprototype(GDIWworklistBoth, true, true )
 
-for _, vin in pairs(GDIWworklistBoth) do
-  -- Determine new names
-  newName = vin.r .. "-GDIW-BR"
-  -- copy table
-  data.raw.recipe[newName] = util.table.deepcopy(data.raw.recipe[vin.r])
-  
-  vro = data.raw.recipe[vin.r]
-  vrn = data.raw.recipe[newName]
-  -- fix names (and enabled for testing)  
-  vrn.name = newName
-  localised_name = {{"recipe-name." .. vro.name},"GDIW.both-reversed"}
-  vrn.enabled = true
-  
-  ingbuild = {}
-  newing = {}
-  fluidflip = {}
-  
-  sortcount = 0
-  for _, vri in pairs(vrn.ingredients) do
-    --Flip Input fluids
-    sortcount = sortcount + 1
-    vri.sortorder = sortcount
-    if vri.type == "fluid" then
-      vri.sortorder = 1000 - sortcount
-    end
-  end  
-  table.sort(vrn.ingredients, function(a,b) return a.sortorder<b.sortorder end)
-  
-  sortcount = 0
-  for _, vrr in pairs(vrn.results) do
-    --Flip Output fluids
-    sortcount = sortcount + 1
-    vrr.sortorder = sortcount
-    if vrr.type == "fluid" then
-      vrr.sortorder = 1000 - sortcount
-    end
-  end  
-  table.sort(vrn.results, function(a,b) return a.sortorder<b.sortorder end)
-  
-  
-  
-  newicons = {}
-  --grab old icon(s)
-  if vro.icon then
-    table.insert(newicons,{icon=vro.icon})
-    oldicon = vro.icon
-  elseif vro.icons then
-    newicons = util.table.deepcopy(vro.icons)
-  else
-    table.insert(newicons,{icon="__GDIW__/graphics/placeholder.png"})  
-  end
-  -- add overlay and use
-  table.insert(newicons,{icon = "__GDIW__/graphics/reverse-overlay-both.png"})
-  vrn.icons = newicons
-  
-  -- handle productivity modules
-  if vin.prod then
-    table.insert(GDIWproductivity,newName)
-  end
-  
-end
 
+-- parse research, find unlocks and add new recipe
+for _, rv in pairs(data.raw["technology"]) do
+  if rv.effects then
+    for _, rev in pairs(rv.effects) do
+      if rev.type == "unlock-recipe" then
+        for wlk, wlv in pairs(GDIWresearch) do
+          if rev.recipe == wlk then
+            for _, newrecipe in ipairs(wlv) do
+              table.insert(rv.effects,{type = "unlock-recipe", recipe = newrecipe})
+            end
+          end
+        end
+      end
+    end
+  end
+end
 
 
 
