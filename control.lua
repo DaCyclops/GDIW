30543d20487d34876453d8ca0290f6df7770df7c
DaCyclops <abcyclops@gmail.com>
2017-04-01 22:59:47 +1100

    Initial working implementation of button
    - Button works, but does nothing
    - Also, making it do something is going to be MUCH harder....

--- control.lua
+++ control.lua
@@ -1,4 +1,4 @@
-function recheck_all_recipes (rf) 
+function recheck_all_recipes(rf) 
 
       for _, research in pairs(rf.technologies) do
         if research.researched then
@@ -14,6 +14,90 @@ function recheck_all_recipes (rf)
 
 end
 
+function checkPlayerForce(pli)
+  if not global.gdiwforce then global.gdiwforce = {} end
+  if not global.gdiwforce[game.players[pli].force.name] then
+    global.gdiwforce[game.players[pli].force.name] = {input = false, output = false}
+  end
+
+end
+
+
+function redraw_button(pli)
+  checkPlayerForce(pli)  
+  local pgt = game.players[pli].gui.top
+  local fb = global.gdiwforce[game.players[pli].force.name]
+  local bs = "NR"
+  if fb.input == true and fb.output == true then
+    bs = "BR"
+  elseif fb.input == true and fb.output == false then
+    bs = "IR"
+  elseif fb.input == false and fb.output == true then
+    bs = "OR"
+  else
+    bs = "NR"
+  end
+    if pgt.gdiw.gdiwsub then 
+      pgt.gdiw.gdiwsub.gdiw_check_input.state = fb.input 
+      pgt.gdiw.gdiwsub.gdiw_check_output.state = fb.output
+    end
+    pgt.gdiw.gdiwbf.gdiwbutton.style = string.sub(pgt.gdiw.gdiwbf.gdiwbutton.style.name, 1, -3)..bs
+end
+
+function createMainButton(pli)
+  checkPlayerForce(pli) 
+  local pgt = game.players[pli].gui.top
+  if not pgt.gdiw then 
+    pgt.add{type = "flow", name = "gdiw", direction = "vertical"} 
+    pgt.gdiw.add{type = "frame", name = "gdiwbf", style = "gdiw_button_frame"} 
+    pgt.gdiw.gdiwbf.add{type = "button", name = "gdiwbutton", style = "gdiw_button_style_C-NR"} 
+    redraw_button(pli)
+  end
+end
+
+
+function onButtonClicked(event)
+  local but = event.element
+  if but.name == "gdiwbutton" then
+    local pli = event.player_index
+    local fb = global.gdiwforce[game.players[pli].force.name]
+    local pgt = game.players[pli].gui.top
+    if pgt.gdiw.gdiwsub then 
+      pgt.gdiw.gdiwsub.destroy()
+      pgt.gdiw.gdiwbf.gdiwbutton.style = string.sub(pgt.gdiw.gdiwbf.gdiwbutton.style.name, 1, -5).."C"..string.sub(pgt.gdiw.gdiwbf.gdiwbutton.style.name, -3, -1)
+    else
+      pgt.gdiw.add{type = "frame", name = "gdiwsub", direction = "vertical"} 
+      pgt.gdiw.gdiwsub.add{type = "checkbox", name = "gdiw_check_input", caption = "Reverse Inputs", state = fb.input} 
+      pgt.gdiw.gdiwsub.add{type = "checkbox", name = "gdiw_check_output", caption = "Reverse Outputs", state = fb.output} 
+      pgt.gdiw.gdiwbf.gdiwbutton.style = string.sub(pgt.gdiw.gdiwbf.gdiwbutton.style.name, 1, -5).."O"..string.sub(pgt.gdiw.gdiwbf.gdiwbutton.style.name, -3, -1)
+    end    
+  end
+end
+
+
+local function onCheckedState(event)
+  local cse = event.element
+  if cse.name == "gdiw_check_input" or cse.name == "gdiw_check_output" then
+    local pli = event.player_index
+    local pfo = game.players[pli].force
+    if not global.gdiwforce[pfo.name] then
+      global.gdiwforce[pfo.name] = {input = false, output = false}
+    end
+    local fst = global.gdiwforce[pfo.name]
+    if cse.name == "gdiw_check_input" then
+      fst.input = cse.state
+    end
+    if cse.name == "gdiw_check_output" then
+      fst.output = cse.state
+    end    
+    
+    for i,_ in pairs (pfo.players) do
+      redraw_button(i)
+    end
+    
+  end
+  
+end
 
 
  -- Start OnLoad/OnInit/OnConfig events
@@ -27,8 +111,7 @@ script.on_configuration_changed( function(data)
     gdiwliststring = loadstring(game.entity_prototypes["gdiw_data_list_flying-text"].order)()
     global.gdiwlist = gdiwliststring
   end
-  
-  
+    
    -- Do GDIW Mod added 
   if data.mod_changes ~= nil and data.mod_changes["GDIW"] ~= nil and data.mod_changes["GDIW"].old_version == nil then
     for _,force in pairs(game.forces) do
@@ -37,7 +120,10 @@ script.on_configuration_changed( function(data)
       
       recheck_all_recipes(force)
     end
-    
+    for idx, _ in pairs(game.players) do
+			createMainButton(idx)
+		end
+
   end 
 
    -- Do GDIW Mod updated or removed
@@ -46,18 +132,22 @@ script.on_configuration_changed( function(data)
       force.reset_recipes()
       force.reset_technologies()
 
-      -- Re-disable all recipes (Useful when old save versions involved)
+      -- Re-disable all GDIW recipes (Useful when old save versions involved)
       for _, i in pairs(global.gdiwlist) do 
         for _, j in pairs(i) do
           force.recipes[j.name].enabled = false
         end
       end
-      
       recheck_all_recipes(force)
-    end
+    end      
 
+    for idx, _ in pairs(game.players) do
+			createMainButton(idx)
+		end
 
+	
   end
+    
 
 
 end)
@@ -65,19 +155,27 @@ end)
 
 script.on_init(function()
   -- Nothing to do now
+
 end)   
   
 script.on_load(function()
   --Nothing to Do Now  
+  
 end)
 -- End OnLoad/OnInit/OnConfig events
 
 
 
 local function onTick(event)
-
-
-  
+  if event.tick == 10 then
+    
+    for idx, _ in pairs(game.players) do
+		 createMainButton(idx)
+		end
+  end
 end
 
 script.on_event(defines.events.on_tick,onTick)
+
+script.on_event(defines.events.on_gui_checked_state_changed,onCheckedState)
+script.on_event(defines.events.on_gui_click,onButtonClicked)
--- prototypes/style.lua
+++ prototypes/style.lua
@@ -1,35 +1,25 @@
-data:extend(
-	{
-	  {
-		type = "font",
-		name = "gdiw-font",
-		from = "default",
-		size = 12
-	  }
-	}
-)
 
-data.raw["gui-style"].default["gdiw_frame_style"] =
+data.raw["gui-style"].default["gdiw_button_frame"] =
 {
 type = "frame_style",
 	parent = "frame_style",
-	width = 33,
-	height = 33,
-	left_padding = 1,
-	right_padding = 1,
-	top_padding = 2,
-	bottom_padding = 2,
+    width = 40,
+    height = 40,
+	left_padding = 0,
+	right_padding = 0,
+	top_padding = 0,
+	bottom_padding = 0,
 }
 
-data.raw["gui-style"].default["gdiw_button_style"] = {
+data.raw["gui-style"].default["gdiw_button_style_C-NR"] = {
     type = "button_style",
     parent = "button_style",
-    width = 33,
-    height = 33,
+    width = 32,
+    height = 32,
     default_graphical_set = {
         type="monolith",
         monolith_image = {
-            filename = "__GDIW__/graphics/gdiw-button-default.png",
+            filename = "__GDIW__/graphics/gdiw-button-closed-NR.png",
             width = 32,
             height = 32
         }
@@ -37,7 +27,7 @@ data.raw["gui-style"].default["gdiw_button_style"] = {
     hovered_graphical_set = {
         type="monolith",
         monolith_image = {
-            filename = "__GDIW__/graphics/gdiw-button-hove-s1.png",
+            filename = "__GDIW__/graphics/gdiw-button-open-NR.png",
             width = 32,
             height = 32
         }
@@ -45,29 +35,227 @@ data.raw["gui-style"].default["gdiw_button_style"] = {
     clicked_graphical_set = {
         type="monolith",
         monolith_image = {
-            filename = "__GDIW__/graphics/gdiw-button-hove-s1.png",
-            width = 32,
-            height = 32
-        }
-    },
-    disabled_graphical_set = {
-        type="monolith",
-        monolith_image = {
-            filename = "__GDIW__/graphics/gdiw-button-default.png",
+            filename = "__GDIW__/graphics/gdiw-button-open-NR.png",
             width = 32,
             height = 32
         }
     }
 }
 
-data.raw["gui-style"].default["gdiw_text"] =
-{
-	type = "label_style",
-	parent = "label_style",
-	width = 160,
-	align = "center",
-	font = "gdiw-font",
-	font_color = {r = 1, g = 1, b = 1}
+data.raw["gui-style"].default["gdiw_button_style_O-NR"] = {
+    type = "button_style",
+    parent = "button_style",
+    width = 32,
+    height = 32,
+    default_graphical_set = {
+        type="monolith",
+        monolith_image = {
+            filename = "__GDIW__/graphics/gdiw-button-open-NR.png",
+            width = 32,
+            height = 32
+        }
+    },
+    hovered_graphical_set = {
+        type="monolith",
+        monolith_image = {
+            filename = "__GDIW__/graphics/gdiw-button-open-NR.png",
+            width = 32,
+            height = 32
+        }
+    },
+    clicked_graphical_set = {
+        type="monolith",
+        monolith_image = {
+            filename = "__GDIW__/graphics/gdiw-button-open-NR.png",
+            width = 32,
+            height = 32
+        }
+    }
 }
 
+data.raw["gui-style"].default["gdiw_button_style_C-IR"] = {
+    type = "button_style",
+    parent = "button_style",
+    width = 32,
+    height = 32,
+    default_graphical_set = {
+        type="monolith",
+        monolith_image = {
+            filename = "__GDIW__/graphics/gdiw-button-closed-IR.png",
+            width = 32,
+            height = 32
+        }
+    },
+    hovered_graphical_set = {
+        type="monolith",
+        monolith_image = {
+            filename = "__GDIW__/graphics/gdiw-button-open-IR.png",
+            width = 32,
+            height = 32
+        }
+    },
+    clicked_graphical_set = {
+        type="monolith",
+        monolith_image = {
+            filename = "__GDIW__/graphics/gdiw-button-open-IR.png",
+            width = 32,
+            height = 32
+        }
+    }
+}
+
+data.raw["gui-style"].default["gdiw_button_style_O-IR"] = {
+    type = "button_style",
+    parent = "button_style",
+    width = 32,
+    height = 32,
+    default_graphical_set = {
+        type="monolith",
+        monolith_image = {
+            filename = "__GDIW__/graphics/gdiw-button-open-IR.png",
+            width = 32,
+            height = 32
+        }
+    },
+    hovered_graphical_set = {
+        type="monolith",
+        monolith_image = {
+            filename = "__GDIW__/graphics/gdiw-button-open-IR.png",
+            width = 32,
+            height = 32
+        }
+    },
+    clicked_graphical_set = {
+        type="monolith",
+        monolith_image = {
+            filename = "__GDIW__/graphics/gdiw-button-open-IR.png",
+            width = 32,
+            height = 32
+        }
+    }
+}
+
+data.raw["gui-style"].default["gdiw_button_style_C-OR"] = {
+    type = "button_style",
+    parent = "button_style",
+    width = 32,
+    height = 32,
+    default_graphical_set = {
+        type="monolith",
+        monolith_image = {
+            filename = "__GDIW__/graphics/gdiw-button-closed-OR.png",
+            width = 32,
+            height = 32
+        }
+    },
+    hovered_graphical_set = {
+        type="monolith",
+        monolith_image = {
+            filename = "__GDIW__/graphics/gdiw-button-open-OR.png",
+            width = 32,
+            height = 32
+        }
+    },
+    clicked_graphical_set = {
+        type="monolith",
+        monolith_image = {
+            filename = "__GDIW__/graphics/gdiw-button-open-OR.png",
+            width = 32,
+            height = 32
+        }
+    }
+}
+
+data.raw["gui-style"].default["gdiw_button_style_O-OR"] = {
+    type = "button_style",
+    parent = "button_style",
+    width = 32,
+    height = 32,
+    default_graphical_set = {
+        type="monolith",
+        monolith_image = {
+            filename = "__GDIW__/graphics/gdiw-button-open-OR.png",
+            width = 32,
+            height = 32
+        }
+    },
+    hovered_graphical_set = {
+        type="monolith",
+        monolith_image = {
+            filename = "__GDIW__/graphics/gdiw-button-open-OR.png",
+            width = 32,
+            height = 32
+        }
+    },
+    clicked_graphical_set = {
+        type="monolith",
+        monolith_image = {
+            filename = "__GDIW__/graphics/gdiw-button-open-OR.png",
+            width = 32,
+            height = 32
+        }
+    }
+}
+
+data.raw["gui-style"].default["gdiw_button_style_C-BR"] = {
+    type = "button_style",
+    parent = "button_style",
+    width = 32,
+    height = 32,
+    default_graphical_set = {
+        type="monolith",
+        monolith_image = {
+            filename = "__GDIW__/graphics/gdiw-button-closed-BR.png",
+            width = 32,
+            height = 32
+        }
+    },
+    hovered_graphical_set = {
+        type="monolith",
+        monolith_image = {
+            filename = "__GDIW__/graphics/gdiw-button-open-BR.png",
+            width = 32,
+            height = 32
+        }
+    },
+    clicked_graphical_set = {
+        type="monolith",
+        monolith_image = {
+            filename = "__GDIW__/graphics/gdiw-button-open-BR.png",
+            width = 32,
+            height = 32
+        }
+    }
+}
+
+data.raw["gui-style"].default["gdiw_button_style_O-BR"] = {
+    type = "button_style",
+    parent = "button_style",
+    width = 32,
+    height = 32,
+    default_graphical_set = {
+        type="monolith",
+        monolith_image = {
+            filename = "__GDIW__/graphics/gdiw-button-open-BR.png",
+            width = 32,
+            height = 32
+        }
+    },
+    hovered_graphical_set = {
+        type="monolith",
+        monolith_image = {
+            filename = "__GDIW__/graphics/gdiw-button-open-BR.png",
+            width = 32,
+            height = 32
+        }
+    },
+    clicked_graphical_set = {
+        type="monolith",
+        monolith_image = {
+            filename = "__GDIW__/graphics/gdiw-button-open-BR.png",
+            width = 32,
+            height = 32
+        }
+    }
+}
 
